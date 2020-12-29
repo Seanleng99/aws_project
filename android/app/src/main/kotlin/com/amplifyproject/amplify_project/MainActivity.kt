08baca9b22f4e830160f9c144ed702f63f119188
Seanleng99 <seanleng99@gmail.com>
2020-12-29 16:16:15 +0800

    Part 1: AWS Amplify

---
+++ android/app/src/main/kotlin/com/amplifyproject/amplify_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.amplifyproject.amplify_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:amplify_core/amplify_core.dart';
+import 'package:flutter/material.dart';
+import 'amplifyconfiguration.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+    routes: {},
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // Give our app awareness about whether we are succesfully connected to the cloud
+  bool _amplifyConfigured = false;
+
+  // Instantiate Amplify
+  Amplify amplifyInstance = Amplify();
+
+  @override
+  void initState() {
+    super.initState();
+
+    // amplify is configured on startup
+    _configureAmplify();
+  }
+
+  void _configureAmplify() async {
+    if (!mounted) return;
+
+    try {
+      await amplifyInstance.configure(amplifyconfig);
+      setState(() {
+        _amplifyConfigured = true;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text('Amplify Core example app'),
+            ),
+            body: ListView(padding: EdgeInsets.all(10.0), children: <Widget>[
+              Center(
+                child: Column(children: [
+                  const Padding(padding: EdgeInsets.all(5.0)),
+                  Text(_amplifyConfigured ? "configured" : "not configured"),
+                ]),
+              )
+            ])));
+  }
+}
